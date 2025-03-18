55da2cc1f768d2502e4547d4ec10242a03fb4d00
nikhil-24s <1234nikhilsolanki@gmail.com>
2025-03-18 11:36:18 +0530

    db changes

--- config/db.js
+++ config/db.js
@@ -1,12 +1,16 @@
-const mongoose = require('mongoose')
+const mongoose = require('mongoose');
 
-const connectToDB = async ()=>{
-    try {
-        await mongoose.connect(process.env.MONGO_URI);
-        console.log('connected to DB');
-    } catch (error) {
-        console.log(`failed to connect with DB : ${error}`);
-    }
-}
+const connectToDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('MongoDB connected');
+  } catch (error) {
+    console.error('MongoDB connection error:', error.message);
+    process.exit(1); // Exit process with failure
+  }
+};
 
-module.exports = connectToDB
+module.exports = connectToDB;
