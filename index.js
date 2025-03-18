397af779793ab1a2e25f781704d15d1a3f36d953
nikhil-24s <1234nikhilsolanki@gmail.com>
2025-03-18 11:27:39 +0530

    cors url change2

--- index.js
+++ index.js
@@ -17,7 +17,12 @@ connectToDB()
 Admin()
 
 // Middleware 
-app.use(cors({origin:'https://courses-frontend-three.vercel.app', credentials: true}))
+app.use(cors({
+    origin: ['https://courses-frontend-three.vercel.app'],
+    credentials: true,
+    methods: ['GET', 'POST', 'PUT', 'DELETE'],
+    allowedHeaders: ['Content-Type', 'Authorization']
+  }))
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser())
